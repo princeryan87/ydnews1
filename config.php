fb9aaae7f1cfef52020517f812dc32c8dc6cbddc
princeryan87 <47706382+princeryan87@users.noreply.github.com>
2019-02-19 14:54:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "866984038339584",
+"sign" => "ce4d75f9ff7c34a98bfbe7f27528fa4a26cc5d2f",
+"token" => "11144850717c8a873bfee2c9ac667798",
+"uuid" => "c5fc01e1-e64c-4f0e-b7db-4a6f19003ded"
+];
